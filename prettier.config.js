5d506eec089243fd41ebcd92fdb32c0cba3d1aed
Diego Turco <dhturco@gmail.com>
2021-10-11 23:47:27 -0300

    added prettier.config

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  arrowParens: "avoid",
+  overrides: [
+    {
+      files: 'package*.json',
+    },
+  ],
+};
